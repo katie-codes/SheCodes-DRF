0dcfb193856f073884ec08b7b1657ef125079e6e
katie-codes <katiepattersondesign@gmail.com>
2020-10-17 11:31:59 +1000

    add crowdfunding react folder with basic page set-up

---
+++ crowdfunding-react/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from "./components/Nav/Nav";
+import HomePage from "./pages/HomePage";
+import ProjectPage from "./pages/ProjectPage";
+import "./App.css";
+
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Nav />
+
+        <Switch>
+          <Route path="/project">
+            <ProjectPage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+
+        </Switch>
+      </div>
+    </Router>
+); 
+}
+
+export default App;
